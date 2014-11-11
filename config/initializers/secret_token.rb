b618a50d95e235cf3ac4f994496ecaf56f2f736c
Eric Franz <efranz@osc.edu>
2014-11-10 19:50:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContainerFillSim::Application.config.secret_key_base = '59f1c83726edcf699074a5937d63a6077c2971238598c79724b5499354ee72e0daac6a3f5180c0bf7e578f3e1452dce841cbd6f86d9ac1a1fa544c09dd3d5002'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ContainerFillSim::Application.config.session_store :cookie_store, key: '_container_fill_sim_session'
