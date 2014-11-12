b92732e30d4cc1f4f42d711e92b69dc6d724bb2a
Eric Franz <efranz@osc.edu>
2014-11-12 09:11:25 -0500

    add crimson files path for uploads

--- app/models/container.rb
+++ app/models/container.rb
@@ -1,7 +1,8 @@
 class Container < ActiveRecord::Base
   [:inlet, :outlet, :walls].each do |f|
-    has_attached_file f
+    has_attached_file f, path: "#{ENV['HOME']}/crimson_files/ContainerFillSim/uploads/:id/#{f}"
     validates_attachment_content_type f, :content_type => /text/
     validates_attachment_file_name f, :matches => [/stl\Z/]
+    validates_presence_of f
   end
 end
