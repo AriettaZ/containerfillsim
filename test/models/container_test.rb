8aed9a03c1842dffdcc902a1d2482e9fe1c0baae
Eric Franz <efranz@osc.edu>
2014-11-13 14:31:35 -0500

    update tests for testing staging
    follow example of unit test test_staging.rb in osc-machete
    we will test using the staging object of the Container provided
    by simple job
    
    note the bad practices in the test - SimpleJob needs to provide
    a better public interface

--- test/models/container_test.rb
+++ test/models/container_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
+require 'tmpdir'
 
 class ContainerTest < ActiveSupport::TestCase
+  
   setup do
-    container = Container.new(name: "test",
+    @container = Container.create(name: "test",
       measurement: "meters",
       fluid_type: "water",
       kinematic_viscosity: 0.000001,
@@ -15,11 +17,34 @@ class ContainerTest < ActiveSupport::TestCase
       outlet: File.new("test/fixtures/control/constant/triSurface/outlet1.stl"),
       walls: File.new("test/fixtures/control/constant/triSurface/walls.stl"),
     )
+    
+    # we render instances of simulations in tmpdir for testing purposes
+    @target = Dir.mktmpdir
+    
+    # the container gives the staging object used
+    @staging = @container.staging
+    @staging.instance_variable_set(:@target, Pathname.new(@target).cleanpath)
+    
+    # the control is what we want to generate
+    @expected = 'test/fixtures/control'
+  end
+  
+  teardown do
+    FileUtils.remove_entry @target
+  end
+  
+  test "paperclip file set works" do
+    assert_equal "inlet1.stl", @container.inlet_file_name
+    assert_equal "outlet1.stl", @container.outlet_file_name
+    assert_equal "walls.stl", @container.walls_file_name
   end
   
   test "staging copies input files" do
   end
   
   test "staging" do
+    job = @staging.new_job(@container.serializable_hash)
+    
+    assert_equal "", `diff -r #{job.path} #{@expected}`
   end
 end
