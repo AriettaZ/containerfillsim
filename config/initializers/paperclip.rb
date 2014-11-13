ae1d9173f6d94de8359b67eb0ec61797142cb22c
Eric Franz <efranz@osc.edu>
2014-11-13 13:50:03 -0500

    fix file uploads to store in crimson_files dir

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,6 +4,5 @@ Paperclip.options[:content_type_mappings] = {
   :stl => "text/plain"
 }
 
-# Paperclip.options[:command_path] = "/usr/bin/"
-# Paperclip::Attachment.default_options[:path] = "#{ENV['HOME']}/crimson_files/ContainerFillSim/uploads/:id/:basename.:extension"
+Paperclip::Attachment.default_options[:path] = "#{ENV['HOME']}/crimson_files/ContainerFillSim/uploads/:id/:attachment/:basename.:extension"
 
