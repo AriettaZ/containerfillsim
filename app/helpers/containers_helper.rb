e15b515fe9afacc6e742a5aafe5087abec0d4cb0
Eric Franz <efranz@osc.edu>
2014-11-14 09:50:13 -0500

    add showing status to index page

--- app/helpers/containers_helper.rb
+++ app/helpers/containers_helper.rb
@@ -1,2 +1,15 @@
 module ContainersHelper
+  def job_status_label(sim)
+    label_class = "label-default"
+    
+    if sim.failed?
+      label_class = "label-danger"
+    elsif sim.completed?
+      label_class = "label-success"
+    elsif sim.running?
+      label_class = "label-primary"
+    end
+    
+    "<span class=\"label #{label_class}\">#{sim.status_human_readable}</span>".html_safe
+  end
 end
