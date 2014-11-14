c0df6ebec08e8e59ee6d21ad80d8b81a307e95fc
Jeremy Nicklas <jnicklas@osc.edu>
2014-11-14 16:46:33 -0500

    fixed file downloads

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,5 +4,9 @@ Paperclip.options[:content_type_mappings] = {
   :stl => "text/plain"
 }
 
-Paperclip::Attachment.default_options[:path] = "#{ENV['HOME']}/crimson_files/ContainerFillSim/uploads/:id/:attachment/:basename.:extension"
+Paperclip::Attachment.default_options[:path] = "#{ENV['HOME']}/crimson_files/ContainerFillSim/uploads/:id/:attachment/:filename"
+Paperclip::Attachment.default_options[:url] = ":rails_relative_url_root/files/uploads/:id/:attachment/:filename"
 
+Paperclip.interpolates :rails_relative_url_root do |attachment, style|
+  Rails.application.config.action_controller.relative_url_root.to_s
+end
