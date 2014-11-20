46ca237a008b9b7a581cf3a279792157d9a851fc
Eric Franz <efranz@osc.edu>
2014-11-20 17:26:33 -0500

    qdel job and cleanup dir on simulation destroy

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ActiveRecord::Base
   include OSC::Machete::SimpleJob::Statusable
   belongs_to :container
+
+  before_destroy { |j| j.job.delete(rmdir: true) }
+  
+  def job
+    OSC::Machete::Job.new(pbsid: pbsid, script: Pathname.new(job_path).join(script_name))
+  end
 end
