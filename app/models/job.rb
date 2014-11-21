bb21bd61fe712f10ba5dc89a334429ffc49f2b09
Eric Franz <efranz@osc.edu>
2014-11-21 09:43:31 -0500

    add shells for methods to handle validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -16,4 +16,33 @@ class Job < ActiveRecord::Base
     self.script_name = new_job.script_name
     self.status = new_job.status
   end
+  
+  def results_valid?
+    # FIXME: magic strings here and in Container.rb!!!
+    return solver_results_valid? if script_name == "runScript.txt"
+    return post_results_valid? if script_name == "gpuRenderScript.txt"
+    
+    raise "No validation method for job with script: #{script_name}"
+  end
+
+  def solver_results_valid?
+    # check LOG-iF
+    # UNIT TESTS ARE GOOD FOR THIS
+    true
+  end
+
+  def post_results_valid?
+    # check for existence of images for each timestep
+    # (list of Pathname objects).all(exist?)
+    # check for existence of filling.gif
+    #
+    # problem with this approach is that to validate the job, we need
+    # the container (or the container's template_presenter) to access
+    # the steps attr
+    #
+    # paths = (0..steps).map { |i| Pathname.new("images/%.2d" % i) }
+    # paths << Pathname.new("movies/filling.gif")
+    # paths.all?(&:exist?)
+    true
+  end
 end
