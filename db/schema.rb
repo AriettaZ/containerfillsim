46cf24abb8fb41ca7577997f7783d7532c417b16
Eric Franz <efranz@osc.edu>
2015-01-06 12:52:29 -0500

    add data migration to create attachments
    create an attachment for each inlet and outlet of a container

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106161341) do
+ActiveRecord::Schema.define(version: 20150106171020) do
 
   create_table "containers", force: true do |t|
     t.string   "name"
