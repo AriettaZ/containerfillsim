e5622b5429f42c8cfdbed74ade8eeb2c300a040d
Jeremy Nicklas <jnicklas@osc.edu>
2015-01-07 16:38:38 -0500

    fixed paraview to use new osc-vnc format

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -115,7 +115,7 @@ class ContainersController < ApplicationController
 
     outdir = OSC::Machete::Crimson.new("#{Rails.application.class.parent_name}/vnc/paraview").files_path
     xdir = Rails.root.join("jobs", "vnc", "paraview")
-    jnlp = OSC::VNC::Session.new(outdir: outdir, xdir: xdir, cluster: 'glenn', datafile: foam_file).run.to_jnlp
+    jnlp = OSC::VNC::Session.new(outdir: outdir, xdir: xdir, cluster: 'glenn', env_vars: { datafile: foam_file }).run.to_jnlp
 
     send_data jnlp.force_encoding('binary'), type: :jnlp, disposition: "attachment", filename: "paraview.jnlp"
   end
