c59f28c1d8b89b8740f1fea191b3e426e0912727
Eric Franz <efranz@osc.edu>
2015-02-27 11:23:08 -0500

    update status label example
    show example of showing the status for each job
    along side the overall status of the simulation
    using an updated helper method which now
    handles nil as an argument

--- Gemfile
+++ Gemfile
@@ -45,9 +45,10 @@ gem "paperclip", "~> 4.2"
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'autoprefixer-rails'
 
-# gem 'osc-machete', :path => "/nfs/17/efranz/dev/osc-machete"
-# gem 'osc-machete', :git => "git@github.com:AweSim-OSC/osc-machete.git"
-gem 'osc-machete', '~> 0.3.1'
+# gem 'osc-machete', :path => "/nfs/17/efranz/dev/osc-machete" # local
+# gem 'osc-machete', :git => "git@github.com:AweSim-OSC/osc-machete.git", :branch => "master" # master
+# gem 'osc-machete', :git => "git@github.com:AweSim-OSC/osc-machete.git", :tag => "v0.3.2" # tag
+gem 'osc-machete', '~> 0.4.0'
 gem 'osc-vnc', '~> 0.0.1'
 gem 'mocha', :require => false, group: [:test]
 
--- app/helpers/containers_helper.rb
+++ app/helpers/containers_helper.rb
@@ -1,15 +1,20 @@
 module ContainersHelper
-  def job_status_label(sim)
+  def job_status_label(job)
     label_class = "label-default"
-    
-    if sim.failed?
-      label_class = "label-danger"
-    elsif sim.completed?
-      label_class = "label-success"
-    elsif sim.running_queued_or_hold?
-      label_class = "label-primary"
+    status = "Not Submitted"    
+
+    if job
+      status = job.status_human_readable
+      
+      if job.failed?
+        label_class = "label-danger"
+      elsif job.completed?
+        label_class = "label-success"
+      elsif job.active?
+        label_class = "label-primary"
+      end
     end
     
-    "<span class=\"label #{label_class}\">#{sim.status_human_readable}</span>".html_safe
+    "<span class=\"label #{label_class}\">#{status}</span>".html_safe
   end
 end
