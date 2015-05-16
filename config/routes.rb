ae4960324564ca730ea352b17a843dca3bdbb439
Eric Franz <efranz@osc.edu>
2015-05-16 16:19:02 -0400

    remove rack app that is now provided by awesim_rails

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,6 @@ ContainerFillSim::Application.routes.draw do
 
   root 'containers#index'
 
-  #FIXME: add as a separate object that we can mount in osc-machete-rails
-  mount proc { |env| Rack::Directory.new(AwesimRails.dataroot).call(env) } => "/files"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
