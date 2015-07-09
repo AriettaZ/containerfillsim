ab0d7915787860e190e3712f0ca19a291bb45e72
Eric Franz <efranz@osc.edu>
2015-07-09 12:05:22 -0400

    add methods to get job log files

--- app/models/job.rb
+++ app/models/job.rb
@@ -12,17 +12,47 @@ class Job < ActiveRecord::Base
   # Setter that accepts an OSC::Machete::Job instance
   def job=(new_job)
     self.pbsid = new_job.pbsid
-    self.job_path = new_job.path.to_s 
+    self.job_path = new_job.path.to_s
     self.script_name = new_job.script_name
     self.status = new_job.status
   end
-  
+
+  # return string "3950166" whether the pbsid
+  # is "3950166" or "3960166.oak-batch.osc.edu"
+  def pbsid_number
+    pbsid =~ /(\d+)(\..*)?/
+    $1
+  end
+
+  # FIXME: a useful method for any file relative to the job dir path?
+  # FIXME: a null path object would be useful so
+  # calling read on it doesn't throw an error
+  def output_file_path
+    path = Pathname.new(job_path).join("*.o#{pbsid_number}")
+    Pathname.glob(path).first # || Pathname.new("")
+  end
+
+  def error_file_path
+    path = Pathname.new(job_path).join("*.e#{pbsid_number}")
+    Pathname.glob(path).first
+  end
+
+  def output_file_contents
+    path = output_file_path
+    path ? path.read : ""
+  end
+
+  def error_file_contents
+    path = error_file_path
+    path ? path.read : ""
+  end
+
   # timesteps value should not be off from the request steps
   # by more than 0.001
   def main_results_valid?
     (container.steps - final_timestep_for_log).abs <= 0.001
   end
-  
+
   # the LOG-iF file contains lines in the form:
   # Time = 4.99853
   # Time = 4.99902
@@ -31,7 +61,7 @@ class Job < ActiveRecord::Base
   def final_timestep_for_log
     path = Pathname.new(self.job_path).join("LOG-iF")
     timestep=0.0
-    
+
     if path.exist? && path.readable?
       path.each_line do |line|
         # iterate through each line in order; assume final match is the largest
@@ -40,7 +70,7 @@ class Job < ActiveRecord::Base
         end
       end
     end
-    
+
     timestep
   end
 
@@ -49,7 +79,7 @@ class Job < ActiveRecord::Base
     # check for existence of filling.gif
     paths = (0..(container.steps)).map { |i| Pathname.new(job_path).join("images/t%.2d.png" % i) }
     paths << Pathname.new(job_path).join("movies/filling.gif")
-    
+
     paths.all?(&:exist?)
   end
 end
