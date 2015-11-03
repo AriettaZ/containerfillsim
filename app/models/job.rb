6f4cd327f69c476b827ef855607bdeda783489a2
Eric Franz <efranz@osc.edu>
2015-11-03 11:07:09 -0500

    delete duplicate code

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,19 +4,6 @@ class Job < ActiveRecord::Base
 
   before_destroy { |j| j.job.delete(rmdir: true) }
 
-  # Returns associated OSC::Machete::Job instance
-  def job
-    OSC::Machete::Job.new(pbsid: pbsid, script: Pathname.new(job_path).join(script_name))
-  end
-
-  # Setter that accepts an OSC::Machete::Job instance
-  def job=(new_job)
-    self.pbsid = new_job.pbsid
-    self.job_path = new_job.path.to_s
-    self.script_name = new_job.script_name
-    self.status = new_job.status
-  end
-
   # return string "3950166" whether the is "3950166" or "3960166.oak-batch.osc.edu"
   def pbsid_number
     pbsid =~ /(\d+)(\..*)?/
