affdcd80012dfa16bec0f16739aeda4d523b9644
Eric Franz <efranz@osc.edu>
2016-12-02 18:26:16 -0500

    inline generator code

--- app/generators/workflows/containers_generator.rb
+++ app/generators/workflows/containers_generator.rb
@@ -1,5 +1,32 @@
 module Workflows
-  class ContainersGenerator < OodJobRails::Generators::WorkflowBase
+  class ContainersGenerator < Rails::Generators::Base
+    def self.default_source_root
+      Rails.root.join('templates', generator_name)
+    end
+
+    def self.desc(description = nil)
+      return super if description
+
+      @desc ||= if usage_path
+                  ERB.new(File.read(usage_path)).result(binding)
+                else
+                  "Description:\n    Stages files for the #{generator_name} workflow."
+                end
+    end
+
+    argument :model_id, type: :string
+
+    def initialize(*args)
+      super
+      self.destination_root = model.root
+    end
+
+    no_tasks do
+      def model
+        @model ||= self.class.generator_name.classify.constantize.find model_id
+      end
+    end
+
     def stage_container
       directory ".", model.root
     end
